c03c5f425eccc1472bd185f8797dee2e517e0a1a
ousemaakacha <akachaousema@gmail.com>
2025-10-17 14:35:45 +0200

    creation of variables with copilot

--- SNACK1/js/snack.js
+++ SNACK1/js/snack.js
@@ -1,1 +1,9 @@
 console.log("SNACK1");
+
+// creaiamo array
+const bici = [
+  { nome: "Bianchi Sprint", peso: 8 },
+  { nome: "Trek Domane", peso: 6.8 },
+  { nome: "Specialized Tarmac", peso: 6.5 },
+  { nome: "Cannondale SuperSix", peso: 7.0 },
+];
